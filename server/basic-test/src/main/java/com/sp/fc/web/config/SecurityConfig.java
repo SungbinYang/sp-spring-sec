0ba2f54c4446e0054ed1f7eb26f90861d43c9278
SungbinYang <ysb05222@naver.com>
2021-08-22 19:08:29 +0900

    스프링 시큐리티 큰 그림

--- server/basic-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/basic-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -34,6 +34,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        http.antMatcher("/api/**");
+
         http.authorizeRequests((requests) ->
             requests.antMatchers("/").permitAll()
                     .anyRequest().authenticated()
