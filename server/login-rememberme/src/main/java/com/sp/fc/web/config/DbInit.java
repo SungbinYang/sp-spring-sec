c53b53ab05c0cc2c38760add110e0be23aa5a3d6
SungbinYang <ysb05222@naver.com>
2021-08-25 05:48:17 +0900

    로그인을 지원하는 필터 & RememberMe 토큰 저장

---
+++ server/login-rememberme/src/main/java/com/sp/fc/web/config/DbInit.java
@@ -0,0 +1,46 @@
+package com.sp.fc.web.config;
+
+import com.sp.fc.user.domain.SpUser;
+import com.sp.fc.user.service.SpUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DbInit implements InitializingBean {
+
+
+    private final SpUserService userService;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        if(!userService.findUser("user1").isPresent()){
+            SpUser user = userService.save(SpUser.builder()
+                    .email("user1")
+                    .password("1111")
+                    .enabled(true)
+                    .build());
+            userService.addAuthority(user.getUserId(), "ROLE_USER");
+        }
+        if(!userService.findUser("user2").isPresent()){
+            SpUser user = userService.save(SpUser.builder()
+                    .email("user2")
+                    .password("2222")
+                    .enabled(true)
+                    .build());
+            userService.addAuthority(user.getUserId(), "ROLE_USER");
+        }
+        if(!userService.findUser("admin").isPresent()){
+            SpUser user = userService.save(SpUser.builder()
+                    .email("admin")
+                    .password("3333")
+                    .enabled(true)
+                    .build());
+            userService.addAuthority(user.getUserId(), "ROLE_ADMIN");
+        }
+    }
+
+
+
+}
--- server/login-userdetails/src/main/java/com/sp/fc/web/UserDetailsTestApplication.java
+++ server/login-userdetails/src/main/java/com/sp/fc/web/UserDetailsTestApplication.java
@@ -2,18 +2,10 @@ package com.sp.fc.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
-        "com.sp.fc.user",
+        "com.sp.fc.config",
         "com.sp.fc.web"
-})
-@EntityScan(basePackages = {
-        "com.sp.fc.user.domain"
-})
-@EnableJpaRepositories(basePackages = {
-        "com.sp.fc.user.repository"
 })
 public class UserDetailsTestApplication {
 
